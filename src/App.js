9a6f933daff5a71966be5a2ba1fabdccc77f6069
abdullahbader <abdallah.b.b96@gmail.com>
2021-06-01 10:36:40 +0300

    first project

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+<div>
+  <h1 className = "App-header" > THINGS CENTRE </h1>     
+  <P>
+    this is my first task
+  </P>
+
+    <img src="https://www.makdigitaldesign.com/wp-content/uploads/2020/02/ecommerce-1.png" />
+    </div>
+  );
+}
+
+export default App;
